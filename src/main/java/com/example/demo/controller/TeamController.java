823d556e3aa10f9f8732eb94b3623ca7f2243dc7
Kim-Taesu <dkdldoafotn@naver.com>
2020-01-12 04:10:30 +0900

    update code

--- src/main/java/com/example/demo/controller/TeamController.java
+++ src/main/java/com/example/demo/controller/TeamController.java
@@ -7,7 +7,6 @@ import com.example.demo.service.TeamService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
